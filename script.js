d273ff50cc8903ec91e1f128b61b9d406899cc6a
Carter Schafer <carter.schafer8@outlook.com>
2024-06-02 18:51:02 -0500

    make project npm

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+type = "module";
+import dotenv from "dotenv";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+dotenv.config();
+
+function formSubmitted() {
+  //   alert("Form submitted!");
+  document.getElementById("preview").style.display = "none";
+}
+
 document
   .getElementById("fileInput")
   .addEventListener("change", function (event) {
@@ -17,6 +27,26 @@ document
   .getElementById("uploadForm")
   .addEventListener("submit", function (event) {
     event.preventDefault();
-    // Here you can handle the form submission, e.g., sending the file to a server
-    alert("Form submitted!");
+    formSubmitted();
   });
+
+// Fetch your API_KEY
+const API_KEY = process.env.GEMINI_API_KEY;
+console.log(API_KEY);
+
+// Access your API key (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(API_KEY);
+
+// The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+async function run() {
+  const prompt = "Write a story about a magic backpack.";
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
+
+run();
