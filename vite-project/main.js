beaec7ab6dadb35d19897558e418b9c5913e9f14
Carter Schafer <carter.schafer8@outlook.com>
2024-06-03 23:11:41 -0500

    make vite project

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,10 +1,13 @@
-import dotenv from "dotenv";
 import { GoogleGenerativeAI } from "@google/generative-ai";
-dotenv.config();
+
+let uploadedImg = null;
 
 function formSubmitted() {
   //   alert("Form submitted!");
-  document.getElementById("preview").style.display = "none";
+  if (uploadedImg) {
+    document.getElementById("preview").style.display = "none";
+    readWithGemini(uploadedImg);
+  }
 }
 
 document
@@ -17,7 +20,7 @@ document
         const preview = document.getElementById("preview");
         preview.src = e.target.result;
         preview.style.display = "block";
-        readWithGemini(file);
+        uploadedImg = file;
       };
       reader.readAsDataURL(file);
     }
@@ -29,6 +32,16 @@ document
     event.preventDefault();
     formSubmitted();
   });
+
+function parseText(text) {
+  // console.log(text);
+  const dataString = text.split("], [");
+  const items = JSON.parse(dataString[0] + "]");
+  const prices = JSON.parse("[" + dataString[1]);
+
+  console.log("Items:", items);
+  console.log("Prices:", prices);
+}
 
 const API_KEY = import.meta.env.VITE_API_KEY;
 const genAI = new GoogleGenerativeAI(API_KEY);
@@ -49,8 +62,9 @@ async function fileToGenerativePart(file) {
 }
 
 async function readWithGemini(img) {
+  console.log("Sending request to Gemini...");
   const prompt =
-    "List the items of this receipt in one column and the prices in another";
+    "Make two arrays. One has the items of this receipt, and the other has the prices. Do not include variable names or anything else, just the arrays.";
 
   // const fileInputEl = document.querySelector("input[type=file]");
   // const imageParts = await Promise.all(
@@ -58,12 +72,18 @@ async function readWithGemini(img) {
   // );
   // const result = await model.generateContent([prompt, ...imageParts]);
 
-  const result = await model.generateContent([
-    prompt,
-    fileToGenerativePart(img),
-  ]);
+  // const result = await model.generateContent([
+  //   prompt,
+  //   fileToGenerativePart(img),
+  // ]);
+
+  const imgArr = [img];
+
+  const imageParts = await Promise.all([...imgArr].map(fileToGenerativePart));
+  const result = await model.generateContent([prompt, ...imageParts]);
 
   const response = await result.response;
   const text = response.text();
-  console.log(text);
+  // console.log(text);
+  parseText(text);
 }
